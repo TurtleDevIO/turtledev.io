2984a832dbbc933a304f38c7a93ec419ae505e6a
Harun Zafer <harunzafer.dev@gmail.com>
2026-05-11 11:14:22 -0400

    Fix date formatting bug

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,5 +1,6 @@
 export const formatDate = (dateString: string) => {
-	const date = new Date(dateString);
+	const [year, month, day] = dateString.split('-').map(Number);
+	const date = new Date(year, month - 1, day);
 	return date.toLocaleDateString('en-US', {
 		month: 'long',
 		day: 'numeric',
